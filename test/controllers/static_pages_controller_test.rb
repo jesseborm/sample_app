6505f6e81928551477feae10c62f590d6c673968
jesseborm <jesseborm@gmail.com>
2017-04-12 10:21:46 +0200

    Add contact page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -24,11 +24,11 @@ end
     assert_select "title", "About | #{@base_title}"
   end
 
-  # test "should get contact" do
-  #   get static_pages_contact_url
-  #   assert_response :success
-  #   assert_select "title", "Contact | #{@base_title}"
-  # end
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
 
 
 end
