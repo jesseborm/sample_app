ec2ae694e45866589f540b3b1fe5ed719ac09462
jesseborm <jesseborm@gmail.com>
2017-05-05 16:38:31 +0200

    Add Sessions controller and login form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)  #temporary implementation
+    @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      redirect_to @user  # === user_url(@user)
     else
       render 'new'
     end
